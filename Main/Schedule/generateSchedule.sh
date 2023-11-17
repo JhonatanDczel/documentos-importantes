b6d8f182e23ada6ab68833ab6078be1f49e94449
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-17 10:51:26 -0500

    Reorganiza los archivos y reescrtibe sus nombres

--- Main/Schedule/generateSchedule.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source myenv/bin/activate
-sed 's/x/ /g' ./sourceSchedule.csv | csvlook | lolcat
-sed 's/x/ /g' ./sourceSchedule.csv | csvlook >Schedule.txt
-sed 's/x/ /g' ./sourceSchedule.csv | csvlook >~/Scripts/Resources/Schedule/Schedule.txt
